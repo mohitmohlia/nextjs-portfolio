91eccf72a598eadac1804b93352a121621ed9441
Mohit Mohlia <mohitmohlia@Mohits-MacBook-Pro.local>
2022-07-29 15:30:46 +0530

    Timeline added for work experience and eduction

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import workExperienceData from "../../data/workExperience.json";
+
+import style from "./projects.module.scss";
 
 function Projects() {
+  console.log({ workExperienceData });
   return (
-    <div style={{ height: "100vh" }} id="projects">
-      Projects
+    <div id="projects" className={style.projects_container}>
+      <h1 className={style.projects_heading}>Work Experience </h1>
+      <VerticalTimeline className={style.verticalTimeline} animate={false}>
+        {workExperienceData.map((data) => {
+          return (
+            <VerticalTimelineElement date={data.timeline}>
+              <h1 className="heading">{data.heading}</h1>
+              <h3 className="style.subHeading">{data.subHeading}</h3>
+              <p className="intro">{data.intro}</p>
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
     </div>
   );
 }
